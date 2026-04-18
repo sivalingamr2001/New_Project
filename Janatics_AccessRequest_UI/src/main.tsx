b9b5698853e7cfe381dde5759551d02ae85ada43
Sivalingam R <sivalinga1902@gmail.com>
2026-04-18 14:10:02 +0530

    initial commit

--- Janatics_AccessRequest_UI/src/main.tsx
+++ Janatics_AccessRequest_UI/src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client"
 
 import "./index.css"
 import App from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
-import { TooltipProvider } from "./components/ui/tooltip.tsx"
+import { ThemeProvider } from "@/providers/theme-provider.tsx"
+import { TooltipProvider } from "./shared/components/ui/tooltip.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
